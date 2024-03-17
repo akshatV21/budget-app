385005a57a991c0ee8fff704f9419d5333f2c69f
akshatV21 <a.vishwakarma2110@gmail.com>
2024-03-17 22:19:27 +0530

    Added register/login routes + Added user modifications migration

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,21 @@
-import { Controller, Post } from '@nestjs/common'
+import { Body, Controller, Post } from '@nestjs/common'
 import { AuthService } from './auth.service'
+import { AuthDto } from './dtos/auth.dto'
+import { HttpResponse } from 'src/utils/types'
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  async httpRegister(@Body() authDto: AuthDto): HttpResponse {
+    const user = await this.authService.register(authDto)
+    return { success: true, message: 'user registered successfully.', data: { user } }
+  }
+
+  @Post('login')
+  async httpLogin(@Body() authDto: AuthDto): HttpResponse {
+    const payload = await this.authService.login(authDto)
+    return { success: true, message: 'User logged in successfully.', data: payload }
+  }
 }
